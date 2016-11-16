58b84ccff8fb583738b8de7fda670890e79eb8cc
Akiko Okabe <aki0u0@gmail.com>
2016-11-15 17:17:56 -0700

    temp

--- src/js/config.js
+++ src/js/config.js
@@ -12,36 +12,34 @@
     $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: './js/components/main/main.view.html',
+      templateUrl: 'js/components/main/main.view.html',
       controller: 'mainController',
       controllerAs: 'mainCtrl',
       restricted: false
     })
     .state('member', {
-      abstract: true,
       url: '/member',
-      templateUrl: './js/components/member/member.view.html',
+      templateUrl: 'js/components/member/member.view.html',
       controller: 'memberController',
       controllerAs: 'memberCtrl',
       restricted: true
     })
     .state('member.info', {
-      url: '/_info',
-      templateUrl: './js/components/member/partials/_info.html',
+      templateUrl: 'js/components/member/partials/_info.html',
       controller: 'memberController',
       controllerAs: 'memberCtrl',
       restricted: true
     })
     .state('login', {
       url: '/login',
-      templateUrl: './js/components/auth/login.view.html',
+      templateUrl: 'js/components/auth/login.view.html',
       controller: 'authController',
       controllerAs: 'authCtrl',
       restricted: false
     })
     .state('register', {
       url: '/register',
-      templateUrl: './js/components/auth/register.view.html',
+      templateUrl: 'js/components/auth/register.view.html',
       controller: 'authController',
       controllerAs: 'authCtrl',
       restricted: false
